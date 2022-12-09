66c620c1c6c4df69c7dc7cd115ee1865c74c2830
Andheoo <118645459+Andheoo@users.noreply.github.com>
2022-12-09 12:15:51 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-36526f722e6645728b450c03eea6fcf4}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
